25efa411400eedce25e1dfe27548b326a37c932f
Louise <louise@zanier.org>
2021-11-08 22:24:35 +0100

    First commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1 class="ui header">Doot</h1>
+  <div class="ui">
+    <Task name="Tâche 1" />
+    <Task name="Tâche 2" />
+  </div>
+  <div class="add">
+    <button class="ui fluid button">Add Task</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Task from "./components/Task.vue";
+
+@Options({
+  components: {
+    Task,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  @media (min-width: 720px) {
+    max-width: 40%;
+  }
+
+  margin: 0 auto;
+  padding-top: 1em;
+}
+
+.header {
+  text-align: center;
+}
+
+.add {
+  margin-top: 1em;
+}
+</style>
---
+++ frontend/src/components/Task.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="task ui fluid card">
+    <div class="content">
+      <div class="header">{{ name }}</div>
+    </div>
+    <div class="ui bottom attached button">Remove task</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    name: String,
+  },
+})
+export default class task extends Vue {
+  name!: string;
+}
+</script>
