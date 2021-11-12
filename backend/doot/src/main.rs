4c449055ca6d9d7ce73d5f25a00c86e1656b981a
Louise <louise@zanier.org>
2021-11-12 11:11:15 +0100

    [backend] Added logs

--- backend/doot/src/main.rs
+++ backend/doot/src/main.rs
@@ -1,9 +1,12 @@
 mod endpoints;
 
+use tide::log;
 use crate::endpoints::get_tasks;
 
 #[tokio::main]
 async fn main() -> tide::Result<()> {
+    log::start();
+
     let mut app = tide::new();
     app.at("/api/tasks").get(get_tasks);
     app.listen("0.0.0.0:8080").await?;
